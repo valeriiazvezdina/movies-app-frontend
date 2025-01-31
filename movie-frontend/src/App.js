cb1a1bbf6aa5d8ee6726636dd5b177f21094c958
valeriia-zvezdina <valeriia.zvezdina@sentinelone.com>
2025-01-31 14:49:31 +0100

    feat: add one more endpoint

--- movie-frontend/src/App.js
+++ movie-frontend/src/App.js
@@ -5,6 +5,8 @@ import { useState, useEffect } from 'react';
 function App() {
 
   const [movies, setMovies] = useState();
+  const [movie, setMovie] = useState();
+  const [reviews, setReviews] = useState([]);
 
   const getMovies = async () => {
     try {
@@ -12,9 +14,24 @@ function App() {
       setMovies(response.data);
     }
     catch (err) {
-      console.log(err);
+      console.error(err);
     }
-  }
+  };
+
+  const getMovieByImdbId = async (imdbId) => {
+
+    try {
+      const response = await api.get(`/movies/${imdbId}`);
+      const singleMovie = response.data;
+
+      setMovie(singleMovie);
+      setReviews(singleMovie.reviews);
+    }
+    catch (error) {
+      console.error(error);
+    }
+
+  };
 
   useEffect(() => {
     getMovies();
