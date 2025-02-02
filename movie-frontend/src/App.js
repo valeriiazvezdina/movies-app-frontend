14d602d0f90a43c88e9182270bebdbe8d10262cd
Valeriia Zvezdina <valeriia.zvezdina@gmail.com>
2025-02-02 09:17:05 +0100

    feat: fix not dynamically changing movie inside the modal

--- movie-frontend/src/App.js
+++ movie-frontend/src/App.js
@@ -1,62 +1,67 @@
+import { useEffect, useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import api from './api/axiosConfig';
-import { useState, useEffect } from 'react';
 import Layout from './components/Layjout';
-import { Routes, Route } from 'react-router-dom';
-import Home from './components/home/Home';
 import Header from './components/header/Header';
-import Trailer from './components/trailer/Trailer';
-import Reviews from './components/reviews/Reviews';
+import Home from './components/home/Home';
 import NotFound from './components/not-found/NotFound';
+import Reviews from './components/reviews/Reviews';
+import Trailer from './components/trailer/Trailer';
 
 function App() {
+	const [movies, setMovies] = useState();
+	const [movie, setMovie] = useState();
+	const [reviews, setReviews] = useState([]);
 
-  const [movies, setMovies] = useState();
-  const [movie, setMovie] = useState();
-  const [reviews, setReviews] = useState([]);
+	const getMovies = async () => {
+		try {
+			const response = await api.get('/movies');
+			setMovies(response.data);
+		} catch (err) {
+			console.error(err);
+		}
+	};
 
-  const getMovies = async () => {
-    try {
-      const response = await api.get('/movies');
-      setMovies(response.data);
-    }
-    catch (err) {
-      console.error(err);
-    }
-  };
+	const getMovieByImdbId = async imdbId => {
+		try {
+			const response = await api.get(`/movies/${imdbId}`);
+			const movie = response.data;
 
-  const getMovieByImdbId = async (imdbId) => {
+			setMovie(movie);
+			setReviews(movie.reviewIds.map(review => review.body));
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
-    try {
-      const response = await api.get(`/movies/${imdbId}`);
-      const movie = response.data;
+	useEffect(() => {
+		getMovies();
+	}, []);
 
-      setMovie(movie);
-      setReviews(movie.reviewIds.map(review => review.body));
-    }
-    catch (error) {
-      console.error(error);
-    }
-
-  };
-
-  useEffect(() => {
-    getMovies();
-  }, []);
-
-  return (
-    <div className="App">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home movies={movies} />} />
-          <Route path="/trailer/:ytTrailerId" element={<Trailer />}></Route>
-          <Route path="/reviews/:imdbId" element={<Reviews getMovieByImdbId={getMovieByImdbId} movie={movie} reviews={reviews} setReviews={setReviews} />}></Route>
-          <Route path="*" element={<NotFound />}></Route>
-        </Route>
-      </Routes>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Header />
+			<Routes>
+				<Route path='/' element={<Layout />}>
+					<Route index element={<Home movies={movies} />} />
+					<Route path='/trailer/:ytTrailerId' element={<Trailer />}></Route>
+					<Route
+						path='/reviews/:imdbId'
+						element={
+							<Reviews
+								getMovieByImdbId={getMovieByImdbId}
+								movie={movie}
+								reviews={reviews}
+								setReviews={setReviews}
+							/>
+						}
+					></Route>
+					<Route path='*' element={<NotFound />}></Route>
+				</Route>
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
--- movie-frontend/src/components/hero/Hero.js
+++ movie-frontend/src/components/hero/Hero.js
@@ -1,57 +1,126 @@
-import './Hero.css';
-import Carousel from 'react-material-ui-carousel';
-import { Paper } from '@mui/material';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
-import { Link, useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Paper } from '@mui/material';
+import React from 'react';
 import Button from 'react-bootstrap/Button';
-
+import Modal from 'react-bootstrap/Modal';
+import Carousel from 'react-material-ui-carousel';
+import { Link, useNavigate } from 'react-router-dom';
+import './Hero.css';
 
 const Hero = ({ movies }) => {
+	const [modalShow, setModalShow] = React.useState(false);
+	const [selectedMovie, setSelectedMovie] = React.useState(null);
 
-    const navigate = useNavigate();
+	const navigate = useNavigate();
 
-    function reviews(imdbId) {
-        navigate(`/reviews/${imdbId}`);
-    }
+	function reviews(imdbId) {
+		navigate(`/reviews/${imdbId}`);
+	}
 
-    return (
-        <div className='movie-carousel-container'>
-            <Carousel>
-                {
-                    movies?.map((movie, imdbId) => (
-                        <Paper key={imdbId}>
-                            <div className='movie-card-container'>
-                                <div className='movie-card' style={{ "--img": `url(${movie.backdrops[0]})` }}>
-                                    <div className='movie-details'>
-                                        <div className='movie-poster'>
-                                            <img src={movie.poster} alt={movie.title} />
-                                        </div>
-                                        <div className='movie-title'>
-                                            <h1>{movie.title}</h1>
-                                        </div>
-                                        <div className="movie-buttons-container">
-                                            <Link to={`/trailer/${movie.trailerLink.substring(movie.trailerLink.length - 11)}`}>
-                                                <div className="play-button-icon-container">
-                                                    <FontAwesomeIcon className="play-button-icon"
-                                                        icon={faCirclePlay}
-                                                    />
-                                                </div>
-                                            </Link>
+	function MyVerticallyCenteredModal(props) {
+		if (!props.movie) return null;
 
-                                            <div className="movie-review-button-container">
-                                                <Button variant="info" onClick={() => reviews(movie.imdbId)} >Reviews</Button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </Paper>
-                    ))
-                }
-            </Carousel>
-        </div>
-    );
-}
+		return (
+			<Modal
+				{...props}
+				size='sm'
+				aria-labelledby='contained-modal-title-vcenter'
+				centered
+			>
+				<Modal.Header closeButton>
+					<Modal.Title id='contained-modal-title-vcenter'>
+						Movie details
+					</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<p>
+						<strong>Genre:</strong> {props.movie.genres.join(', ')}
+					</p>
+					<p>
+						<strong>Release Date:</strong> {props.movie.releaseDate}
+					</p>
+				</Modal.Body>
+				<Modal.Footer>
+					<Button variant='info' onClick={props.onHide}>
+						OK
+					</Button>
+				</Modal.Footer>
+			</Modal>
+		);
+	}
+
+	return (
+		<div className='movie-carousel-container'>
+			<Carousel>
+				{movies?.map((movie, imdbId) => (
+					<Paper key={imdbId}>
+						<div className='movie-card-container'>
+							<div
+								className='movie-card'
+								style={{ '--img': `url(${movie.backdrops[0]})` }}
+							>
+								<div className='movie-details'>
+									<div className='movie-poster'>
+										<img src={movie.poster} alt={movie.title} />
+									</div>
+									<div className='movie-title'>
+										<h1>{movie.title}</h1>
+									</div>
+									<div className='movie-buttons-container'>
+										<Link
+											to={`/trailer/${movie.trailerLink.substring(
+												movie.trailerLink.length - 11
+											)}`}
+										>
+											<div className='play-button-icon-container'>
+												<FontAwesomeIcon
+													className='play-button-icon'
+													icon={faCirclePlay}
+												/>
+											</div>
+										</Link>
+
+										<div className='movie-buttons-reviews-details'>
+											<div className='movie-review-button-container'>
+												<Button
+													variant='info'
+													onClick={() => reviews(movie.imdbId)}
+													style={{ marginBottom: '10px', width: '100px' }}
+												>
+													Reviews
+												</Button>
+											</div>
+
+											<div className='movie-details-button-container'>
+												<Button
+													variant='info'
+													style={{
+														width: '100px',
+													}}
+													onClick={() => {
+														setModalShow(true);
+														setSelectedMovie(movie);
+													}}
+												>
+													Details
+												</Button>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</Paper>
+				))}
+			</Carousel>
+			<MyVerticallyCenteredModal
+				movie={selectedMovie}
+				show={modalShow}
+				onHide={() => setModalShow(false)}
+			/>
+		</div>
+	);
+};
 
 export default Hero;
